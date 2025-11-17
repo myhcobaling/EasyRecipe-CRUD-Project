f2114331358585a0e812c24abaa0341757e9974d
myhcobaling <myhco.baling@cvsu.edu.ph>
2025-11-17 18:48:21 +0800

    added new file

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import HomepageView, AboutpageView, BlogListView
+from .views import (HomepageView, AboutpageView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, )
+
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='home'),
     path('about/', AboutpageView.as_view(), name='about'),
     path('blog/', BlogListView.as_view(), name='blog'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog-update'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
+
+
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,24 +1,52 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView , DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView , UpdateView, DeleteView
+from django.contrib.auth.models import User
 from .models import Post
+from django.urls import reverse_lazy
+
 
 class HomepageView(TemplateView):
     template_name = 'app/home.html'
 
+
 class AboutpageView(TemplateView):
     template_name = 'app/about.html'
 
- class BlogListView(ListView):
-     model = post
-     context_object_name = 'posts'
-    template_name = 'app/blog.html'
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'app/blog_list.html'
+
 
 
 class BlogDetailView(DetailView):
-    model = post
+    model = Post
     context_object_name = 'posts'
-template_name = 'app/blog.html'
+    template_name = 'app/blog_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = 'app/blog_create.html'
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['author'].queryset = User.objects.all()
+        return form
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'app/blog_update.html'
+    fields = ['title', 'author', 'body']
+    success_url = reverse_lazy('blog')
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'app/blog_delete.html'
+    success_url = reverse_lazy('blog')
 
 
 
